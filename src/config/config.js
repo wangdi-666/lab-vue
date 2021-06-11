84f344503ebbafd3cec3097a2aeddd3ed32a2ddb
wangdi <1589955459@qq.com>
2021-06-12 00:09:07 +0800

    修改1

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,3 @@
 export const SYSTEM_NAME = '实验室预约系统';//系统名称
-export const FOOTER_TITLE = 'Copyright© 2021实验室预约系统';//底部标题
+export const FOOTER_TITLE = 'Copyright© 2021实验室预约系统 王迪 甄雅萌';//底部标题
 export const HOME_TITLE = '欢迎使用实验室预约系统';//首页标题
